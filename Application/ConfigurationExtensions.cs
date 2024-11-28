1257810224e6b3c0ba3f00f1891afd80f04a6b8e
Maksim Yurovnik <yurovnik.mv@dns-shop.ru>
2024-11-28 15:52:08 +1000

    Добавил проект веб api и catalog контроллер

---
+++ Application/ConfigurationExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Application.Orders;
+using Application.SaleItem;
+using DAL;
+using DAL.Database;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Application
+{
+    public static class ConfigurationExtensions
+    {
+        public static IServiceCollection RegisterApplicationDependencies(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddScoped<RepositoryFactory>(_ => new DatabaseRepositoryFactory(configuration["ConnectionString"] ?? ""))
+                .AddScoped<GetSaleItemHandler>()
+                .AddScoped<GetCartHandler>()
+                .AddScoped<CreateOrderHandler>()
+                .AddScoped<AddCartLineHandler>();
+
+            return services;
+        }
+    }
+}
---
+++ WebApi/Controllers/CatalogController.cs
@@ -0,0 +1,51 @@
+﻿using Application.SaleItem;
+using Core;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class CatalogController : ControllerBase
+    {
+        private readonly GetSaleItemHandler _saleItemHandler;
+
+        public CatalogController(GetSaleItemHandler getSaleItemHandler)
+        {
+            _saleItemHandler = getSaleItemHandler;
+        }
+
+        /// <summary>
+        /// Получить список продуктов
+        /// </summary>
+        /// <param name="count"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        [HttpGet("products")]
+        public async Task<ActionResult<IEnumerable<SaleItemDto>>> GetProductsAsync([FromQuery] int? count, CancellationToken cancellationToken)
+        {
+            var result = await _saleItemHandler.GetItemsAsync(ItemTypes.Product, count, cancellationToken);
+            if (result.IsFailed)
+                return BadRequest(result.ToString());
+
+            if (!result.Value.Any()) 
+                return NotFound();
+                    
+            return Ok(result.Value);
+        }
+
+        [HttpGet("services")]
+        public async Task<ActionResult<IEnumerable<SaleItemDto>>> GetServicesAsync([FromQuery] int? count, CancellationToken cancellationToken)
+        {
+            var result = await _saleItemHandler.GetItemsAsync(ItemTypes.Service, count, cancellationToken);
+            if (result.IsFailed)
+                return BadRequest(result.ToString());
+
+            if (!result.Value.Any())
+                return NotFound();
+
+            return Ok(result.Value);
+        }
+    }
+}
